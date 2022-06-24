304fb5b564b514687ec2b6ff877156e74840d2e0
Nicholas Grana <grananicholas@gmail.com>
2022-06-24 11:05:29 -0400

    Update config.fish

--- iterm/config.fish
+++ iterm/config.fish
@@ -4,10 +4,10 @@ end
 
 # Terminal
 set -x TERM xterm-256color
+set -xg JAVA_HOME # PLACE JAVA HOME HERE
 
 # Remove fish entrance greeting
 set fish_greeting
 
 # Add iterm2 integration (in iTerm2 do iTerm2 > Install shell integration)
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
